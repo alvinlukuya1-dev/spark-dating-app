db9e2f50635fef36b63a338ea4dd97a5ba3b1fe7
alvinlukuya1-dev <alvinlukuya1-dev@users.noreply.github.com>
2026-07-05 11:59:04 +0300

    fix: suppress Mongoose strictQuery deprecation warning

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,7 @@ import { Post } from './models/Post';
 import { setupSocket, notifyNewMatch } from './socket';
 
 dotenv.config();
+mongoose.set('strictQuery', true);
 
 const app = express();
 const server = http.createServer(app);
